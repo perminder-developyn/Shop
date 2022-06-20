acb8abd84bd2f7e2a9b5e90336560cf0c53166b8
perminder-developyn <perminder.dhillon@developyn.com>
2022-06-20 18:49:19 +0100

    commenting added

--- be-sweets/express.js
+++ be-sweets/express.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const shop = express();
 const port = 5000;
-
+// this stock array contains all information about the stock
 let stock = [
     {
         id: 0,
@@ -22,30 +22,30 @@ let stock = [
         quantity: 2000,
         caseSize: 100
     }
-]
-
+];
+// the express app uses cors settings imported on line 2
 shop.use(cors());
-
+//  a get request that will retrieve the stock array and a message about the call
 shop.get('/stock', (req, res) => {
     let response = {
         data: stock,
         message: "This call has retrieved all stock information."
-    }
-    res.send(response);
+    };
+    res.status(200).send(response);
 });
-
+// a get request that will calculate and send a total value of all stock items
 shop.get('/value', (req, res) => {
     let total = 0;
    stock.forEach((z) => {
     total += (z.quantity * z.price)
-   })
+   });
    let response = {
        data: total.toString(),
        message: "This call has calculated and returned the total value of all stock"
-   }
-   res.send(response)
+   };
+   res.send(response);
 });
-
+// a delete request that will take the id of the item clicked on to be sold and will reduce the item quantity by 1
 shop.delete('/:id', (req, res) => {
     let x = Number(req.params.id)
     stock.forEach(y => {
@@ -56,24 +56,24 @@ shop.delete('/:id', (req, res) => {
     let response = {
         data: stock,
         message: "This call will decrease the stock quantity by 1 and return the resulting stock count"
-    }
-    res.send(response)
+    };
+    res.send(response);
 })
-
+// a post request that will increase the value of the item quantity in question by the case size amount
 shop.post('/:id/restock', (req, res) => {
     let x = Number(req.params.id)
     stock.forEach(y => {
         if (y.id === x){
             y.quantity += y.caseSize;
-        }
-    })
+        };
+    });
     let response = {
         data: stock,
         message: "This call will decrease the stock quantity by 1 and return the resulting stock count"
-    }
-    res.send(response)
+    };
+    res.send(response);
 }) 
-
+// open and listen the server on port defined on line 4
 shop.listen(port, () => {
     console.log(`Example shop listening on port ${port}`);
 });
--- fe-sweets/src/App.vue
+++ fe-sweets/src/App.vue
@@ -31,30 +31,30 @@ export default {
 			this.subTotal();
 			axios.get(`http://localhost:5000/stock`).then(res => {
 				res.data.data.forEach((e)=> {
-					this.shop.push(e)
-				})
-			})
+					this.shop.push(e);
+				});
+			});
 		},
 		sell(e) {
 			axios.delete(`http://localhost:5000/${e.id}`).then(res => {
 				this.shop = [];
 				res.data.data.forEach((e)=> {
-					this.shop.push(e)
-				})
-			})
+					this.shop.push(e);
+				});
+			});
 		},
 		order(e) {
 			axios.post(`http://localhost:5000/${e.id}/restock`).then(res => {
 				this.shop = [];
 				res.data.data.forEach((e) => {
-					this.shop.push(e)
-				})
-			})
+					this.shop.push(e);
+				});
+			});
 		},
 		subTotal() {
 			axios.get(`http://localhost:5000/value`).then(res => {
-				this.total = res.data.data
-			})
+				this.total = res.data.data;
+			});
 		}
 	},
 	data() {
@@ -66,7 +66,7 @@ export default {
 	},
 	watch: {
 		shop() {
-			this.subTotal()
+			this.subTotal();
 		}
 	}
 }
