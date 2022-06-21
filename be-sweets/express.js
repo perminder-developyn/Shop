50283f8ca7526e9fc044fc65b66ed51db9e14940
perminder-developyn <perminder.dhillon@developyn.com>
2022-06-21 10:59:07 +0100

    formatting

--- be-sweets/express.js
+++ be-sweets/express.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const shop = express();
 const port = 5000;
+
 // this stock array contains all information about the stock
 let stock = [
     {
@@ -23,8 +24,10 @@ let stock = [
         caseSize: 100
     }
 ];
+
 // the express app uses cors settings imported on line 2
 shop.use(cors());
+
 //  a get request that will retrieve the stock array and a message about the call
 shop.get('/stock', (req, res) => {
     let response = {
@@ -33,6 +36,7 @@ shop.get('/stock', (req, res) => {
     };
     res.status(200).send(response);
 });
+
 // a get request that will calculate and send a total value of all stock items
 shop.get('/value', (req, res) => {
     let total = 0;
@@ -45,6 +49,7 @@ shop.get('/value', (req, res) => {
    };
    res.send(response);
 });
+
 // a delete request that will take the id of the item clicked on to be sold and will reduce the item quantity by 1
 shop.delete('/:id', (req, res) => {
     let x = Number(req.params.id)
@@ -58,7 +63,8 @@ shop.delete('/:id', (req, res) => {
         message: "This call will decrease the stock quantity by 1 and return the resulting stock count"
     };
     res.send(response);
-})
+});
+
 // a post request that will increase the value of the item quantity in question by the case size amount
 shop.post('/:id/restock', (req, res) => {
     let x = Number(req.params.id)
@@ -72,7 +78,8 @@ shop.post('/:id/restock', (req, res) => {
         message: "This call will decrease the stock quantity by 1 and return the resulting stock count"
     };
     res.send(response);
-}) 
+});
+
 // open and listen the server on port defined on line 4
 shop.listen(port, () => {
     console.log(`Example shop listening on port ${port}`);
