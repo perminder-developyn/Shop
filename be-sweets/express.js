f7fdda557100fec097410abf7e8c1658c2f21013
perminder-developyn <perminder.dhillon@developyn.com>
2022-07-07 14:11:52 +0100

    reverting for simplicity

--- be-sweets/express.js
+++ be-sweets/express.js
@@ -1,45 +1,58 @@
 const express = require('express');
 const cors = require('cors');
-const database = require('./database/connection')
-const shop = express();
+const app = express();
 const port = 8080;
 
-// the express app uses cors settings imported on line 2
-shop.use(cors());
-shop.get('/', (req, res) => {
-    console.log(req, res)
+let stock = [
+    {
+        id: 0,
+        name: 'Candyfloss',
+        description: 'Sweet and tasty sugar',
+        price: 12,
+        currency: 'EUR',
+        quantity: 350,
+        caseSize: 6
+    },
+    {
+        id: 2,
+        name: 'Stroopwaffel',
+        description: 'Caramelly deliciousness',
+        price: 1,
+        currency: 'EUR',
+        quantity: 2000,
+        caseSize: 100
+    }
+]
+
+app.use(cors());
+app.get('/stock', (req, res) => {
+    res.send(stock);
+});
+app.get('/value', (req, res) => {
+    let total = 0;
+   stock.forEach((z) => {
+    total += (z.quantity * z.price)
+   })
+   res.send(total.toString())
+});
+app.delete('/:id', (req, res) => {
+    let x = Number(req.params.id)
+    stock.forEach(y => {
+        if (y.id === x){
+            y.quantity --;
+        }
+    })
+    res.send(stock)
 })
-
-//  a get request that will retrieve the stock array and a message about the call
-shop.get('/stock', (req, res) => {
-    database.get_stock().then(data => {
-
-        let response = {
-            data: data,
-            message: "This call has retrieved all stock information."
-        };
-        res.status(200).send(response);
-    });
-});
-
-// a get request that will calculate and send a total value of all stock items
-shop.get('/value', (req, res) => {
-    database.get_value().then(data => {
-        res.send(data[0].sum);
-    });
-});
-
-// a delete request that will take the id of the item clicked on to be sold and will reduce the item quantity by 1
-shop.delete('/:id', (req) => {
-    database.reduce_item(req.params);
-});
-
-// a post request that will increase the value of the item quantity in question by the case size amount
-shop.post('/:id/restock', (req) => {
-    database.order_item(req.params);
-});
-
-// open and listen the server on port defined on line 4
-shop.listen(port, () => {
-    console.log(`Example shop listening on port ${port}`);
-});
+app.post('/:id/restock', (req, res) => {
+    let x = Number(req.params.id)
+    stock.forEach(y => {
+        if (y.id === x){
+            y.quantity += y.caseSize;
+        }
+    })
+    res.send(stock)
+}) 
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+})
