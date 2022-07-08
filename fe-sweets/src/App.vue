c53ae3c369a0fa9670e9c205a548d0a95609ead8
perminder-developyn <perminder.dhillon@developyn.com>
2022-07-08 16:39:08 +0100

    files

--- fe-sweets/src/App.vue
+++ fe-sweets/src/App.vue
@@ -33,7 +33,7 @@ export default {
 			this.count = 0;
 			this.count ++;
 			this.subTotal();
-			axios.get(`api:8080/stock`).then(res => {
+			axios.get(`api/stock`).then(res => {
 				console.log(res)
 				res.data.forEach((item)=> {
 					this.shop.push(item);
@@ -41,17 +41,17 @@ export default {
 			});
 		},
 		sell(item) {
-			axios.delete(`api:8080/${item.id}`).then(
+			axios.delete(`api/${item.id}`).then(
 				this.updateShop()
 			);
 		},
 		order(item) {
-			axios.post(`api:8080/${item.id}/restock`).then(
+			axios.post(`api/${item.id}/restock`).then(
 				this.updateShop()
 			);
 		},
 		subTotal() {
-			axios.get(`api:8080/value`).then(res => {
+			axios.get(`/value`).then(res => {
 				this.total = res.data;
 			});
 		}
